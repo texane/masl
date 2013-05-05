ef2489a431b8a4e97b7a2a3a8c8d7278889d9627
Fabien Le Mentec <texane@gmail.com>
2013-05-05 16:44:07 -0500

    [ update ] masl test program

--- src/main.c
+++ src/main.c
@@ -2,19 +2,24 @@
 #include <unistd.h>
 #include "masl.h"
 
-static masl_err_t onslave(masl_handle_t* h, unsigned int si, void* p)
-{
-  printf("on slave: %u\n", si);
-  return MASL_ERR_CONTINUE;
-}
-
 int main(int ac, char** av)
 {
   masl_handle_t* h;
+  unsigned int si;
 
   if (masl_init(&h) != MASL_ERR_SUCCESS) return -1;
 
-  masl_loop(h, onslave, NULL, 0);
+  while (1)
+  {
+    if (masl_wait_slave(h, &si, -1) != MASL_ERR_SUCCESS)
+    {
+      printf("masl_wait_slave error\n");
+      break ;
+    }
+
+    printf("on slave: %u\n", si);
+  }
+
   masl_fini(h);
 
   return 0;
